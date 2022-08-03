8460d82b7ff0ebc3cef9e1bbad0e3da915adb17d
Meghan <106887335+Megtails@users.noreply.github.com>
2022-08-03 18:59:35 -0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿Console.WriteLine("How many miles per gallon does your car get?");
+string milesPerGallon = Console.ReadLine();
+double mpg = double.Parse(milesPerGallon);
+
+Console.WriteLine("Please enter a radius:");
+string radius = Console.ReadLine();
+double r = double.Parse(radius);
+
+double area = Math.PI * (r * r);
+double aRounded = Math.Round(area, 3);
+
+double circumference = 2 * Math.PI * r;
+double cRounded = Math.Round(circumference, 3);
+
+double diameter = 2 * r;
+double dRounded = Math.Round(diameter, 3);
+
+double roundTrip = circumference / mpg;
+double rT = Math.Round(roundTrip, 3);
+
+Console.WriteLine("The area of a circle of radius " + radius + " is: " + aRounded);
+Console.WriteLine("The circumference of a circle of radius " + radius + " is: " + cRounded);
+Console.WriteLine("The diameter of a circle of radius " + radius + " is: " + dRounded);
+Console.WriteLine("It would take you " + rT + " gallons of gas to travel once around this circle.");
+Console.WriteLine("Yay!");
